6840195cf553b23a0b53c471a29bbd09ad1e7642
Angela Lindow <angelalindow@gmail.com>
2016-09-20 09:21:28 -0600

    Complete get all items request

--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -10,9 +10,17 @@ RSpec.describe "Items requests", type: :request do
     Item.create(name: "thing2", description: "description2", image_url: "https://robohash.org/2")
 
     get 'api/v1/items'
-    require "pry"; binding.pry
-    expect(json.count).to eq(2)
-    expect(json.first[:name]).to eq("thing1")
-    expect(json.last[:name]).to eq("thing2")
+
+    expect(json(response).count).to eq(2)
+    expect(json(response).first["name"]).to eq("thing1")
+    expect(json(response).first["description"]).to eq("description1")
+    expect(json(response).first["image_url"]).to eq("https://robohash.org/1")
+    expect(json(response).last["description"]).to eq("description2")
+    expect(json(response).last["image_url"]).to eq("https://robohash.org/2")
+    expect(json(response).last["name"]).to eq("thing2")
+  end
+
+  def json(response)
+    JSON.parse(response.body)
   end
 end
