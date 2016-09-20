73af34c33897e18ce4750b6f4d4424301758c8cc
Angela Lindow <angelalindow@gmail.com>
2016-09-20 09:48:57 -0600

    Finish post request for item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -13,4 +13,14 @@ class Api::V1::ItemsController < ApplicationController
       render json: "No content", status: 204
     end
   end
+
+  def create
+    @item = Item.create(item_params)
+    render 'api/v1/items/show'
+  end
+
+  private
+    def item_params
+      params.permit(:name, :description, :image_url)
+    end
 end
